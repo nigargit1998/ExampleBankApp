3afda32a5d2616373e637b29d56fa0f62e209cb0
Mammadova Nigar <004819@ibar.int>
2024-01-11 19:33:32 +0400

    Initial Commit

---
+++ ExampleBankAppUITests/ExampleBankAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ExampleBankAppUITestsLaunchTests.swift
+//  ExampleBankAppUITests
+//
+//  Created by Mammadova Nigar Rauf on 11.01.24.
+//
+
+import XCTest
+
+final class ExampleBankAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
